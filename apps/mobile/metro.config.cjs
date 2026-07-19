b4339b4ec8c84fc035f580ea25f15708470ab91b
David at home <iam.qixin@gmail.com>
2026-07-19 15:05:49 +0800

    fix: use process.cwd() in metro config for correct project root

--- apps/mobile/metro.config.cjs
+++ apps/mobile/metro.config.cjs
@@ -1,10 +1,5 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
 
-const config = getDefaultConfig(__dirname);
-
-// Explicitly tell Metro to resolve modules from the mobile app's node_modules
-// instead of following bun workspace symlinks to the root
-config.resolver.nodeModulesPaths = [path.resolve(__dirname, "node_modules")];
+const config = getDefaultConfig(process.cwd());
 
 module.exports = config;
